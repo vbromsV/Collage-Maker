895fdc3cb347c6a74d82d6ab37a43608f32070dd
vbromsV <vincent.broms@outlook.com>
2025-06-03 17:38:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,37 @@
-const CACHE_NAME = 'collage-cache-v1';
-const FILES_TO_CACHE = [
+const CACHE_NAME = 'collage-mobile-cache-v1';
+const urlsToCache = [
+  './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Clean up old caches if needed
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
   );
 });
